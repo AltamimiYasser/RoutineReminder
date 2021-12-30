912296fdcd17792145dd506a107e825450fea901
yasser tamimi <altamimiy89@gmail.com>
2021-12-31 01:35:14 +0300

    Initial Commit

---
+++ RoutineReminder/RoutineReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutineReminderApp.swift
+//  RoutineReminder
+//
+//  Created by Yasser Tamimi on 31/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct RoutineReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
